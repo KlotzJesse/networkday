31c5390e402c23f2ca11e36f5ea5fe23585ca054
Jesse Klotz <jesseklotz2306@gmail.com>
2021-10-29 12:37:00 +0200

    Cleaning project, activating experimental features

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    concurrentFeatures: true,
+    serverComponents: true,
+    reactRoot: true,
+  },
 }
