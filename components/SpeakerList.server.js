f2518ad7914ee0bca393d1ff14b14b42b4b0e5db
Jesse Klotz <jesseklotz2306@gmail.com>
2021-11-25 00:47:24 +0100

    Added new test

--- components/SpeakerList.server.js
+++ components/SpeakerList.server.js
@@ -4,6 +4,8 @@ import fetchData from "../lib/fetchData";
 export default function SpeakerList() {
   const speakerList = useData("speaker", () => fetchData(`/api/speakerap`));
 
+  console.log(speakerList);
+
   return (
     <>
       {speakerList.map((speaker) => {
