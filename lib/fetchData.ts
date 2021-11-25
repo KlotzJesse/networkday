d70bc3968b254795c098f61cbfb4b24197f1e7f4
Jesse Klotz <jesseklotz2306@gmail.com>
2021-11-25 03:46:27 +0100

    Update

--- lib/fetchData.ts
+++ lib/fetchData.ts
@@ -1,5 +1,5 @@
 export default async function fetchData(type) {
-  const res = await fetch(type);
+  const res = await fetch(process.env.NEXT_PUBLIC_BASE_URL + type);
   if (res.status !== 200) {
     throw new Error(`Status ${res.status}`);
   }
