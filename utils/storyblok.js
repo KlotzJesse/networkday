cc687f7dcf63a20183cb8afa1b5bbcf3fb1d5c70
Jesse Klotz <jesseklotz2306@gmail.com>
2021-11-25 02:42:19 +0100

    Testing server api

--- utils/storyblok.js
+++ utils/storyblok.js
@@ -1,11 +1,15 @@
 import StoryblokClient from "storyblok-js-client";
 
-const Storyblok = new StoryblokClient({
-  accessToken: "wM296GLYg0BtNHBdvqcsuwtt",
-  cache: {
-    clear: "auto",
-    type: "memory",
-  },
-});
+const Storyblok =
+  global.Storyblok ||
+  new StoryblokClient({
+    accessToken: "wM296GLYg0BtNHBdvqcsuwtt",
+    cache: {
+      clear: "auto",
+      type: "memory",
+    },
+  });
+
+if (process.env.NODE_ENV === "development") global.Storyblok = Storyblok;
 
 export default Storyblok;
