692e6b80edef9aee4ec9e8b61769e5e58b9d21f2
Jesse Klotz <jesseklotz2306@gmail.com>
2021-11-25 10:10:36 +0100

    Removed test & news

--- pages/api/news.ts
+++
@@ -1,4 +0,0 @@
-export default async function handler(req, res) {
-    const data = await (await fetch(`https://hacker-news.firebaseio.com/v0/topstories.json`)).json();
-    return res.status(200).json(data.story);
-}
