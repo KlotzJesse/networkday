5da5627f3e3aa8166a6f89c389055428d4ef4665
Jesse Klotz <jesseklotz2306@gmail.com>
2021-11-25 02:46:23 +0100

    Update

--- components/SpeakerList.server.js
+++ components/SpeakerList.server.js
@@ -2,9 +2,7 @@ import useData from "../lib/useData";
 import fetchData from "../lib/fetchData";
 
 export default function SpeakerList() {
-  const speakerList = useData("speaker", () => fetchData(`/api/speakerap`));
-
-  console.log(speakerList);
+  const speakerList = useData("speakeraaaa", () => fetchData(`/api/speakerap`));
 
   return (
     <>
--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import type { AppProps } from "next/app";
-
-import "../styles/globals.css";
-
-function NetworkDayApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-
-export default NetworkDayApp;
