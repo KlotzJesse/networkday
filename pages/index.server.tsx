7c9f654971879411f10f0fa8fe0eef1b3aad2789
Jesse Klotz <jesseklotz2306@gmail.com>
2021-11-25 02:38:54 +0100

    Test

--- pages/index.server.tsx
+++ pages/index.server.tsx
@@ -3,10 +3,9 @@ import useData from "../lib/useData";
 import fetchData from "../lib/fetchData";
 
 export default function Home() {
-  const story = useData("homePage", () => fetchData(`/api/components/home`));
   return (
     <div>
-      <DynamicComponent blok={story.content} />
+      <p>Test</p>
     </div>
   );
 }
