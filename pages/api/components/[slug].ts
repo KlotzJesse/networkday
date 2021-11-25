0dc4a5df8755f4402bb9204eb20cf537abc906e5
Jesse Klotz <jesseklotz2306@gmail.com>
2021-11-25 11:30:33 +0100

    Changing version to draft

--- pages/api/components/[slug].ts
+++ pages/api/components/[slug].ts
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
   const { slug } = req.query;
-  const data = await (await fetch(`https://api.storyblok.com/v1/cdn/stories/${slug}?version=published&token=wM296GLYg0BtNHBdvqcsuwtt`)).json();
+  const data = await (await fetch(`https://api.storyblok.com/v1/cdn/stories/${slug}?version=draft&token=wM296GLYg0BtNHBdvqcsuwtt&cv=${new Date().getTime()}`)).json();
   return res.status(200).json(data.story);
 }
