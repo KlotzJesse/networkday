45ca49788a720b7663de20fa688e5dffb0057117
Jesse Klotz <jesseklotz2306@gmail.com>
2021-11-26 18:17:42 +0100

    Protect pages

---
+++ pages/app/_middleware.js
@@ -0,0 +1,11 @@
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+
+export default async function middleware(req, event) {
+  const session = await getToken({ req, secret: process.env.AUTH_SECRET });
+
+  // You could also check for any property on the session object,
+  // like role === "admin" or name === "John Doe", etc.
+  if (!session) return NextResponse.next();
+  // If user is authenticated, continue.
+}
