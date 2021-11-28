3d30b49444af5445c55b4abeec76bdc5b4808e81
Jesse Klotz <jesseklotz2306@gmail.com>
2021-11-28 19:54:15 +0100

    Changed error Page Logo

--- pages/404.tsx
+++ pages/404.tsx
@@ -93,16 +93,26 @@ const social = [
   },
 ];
 
-export default function Example() {
+export default function ErrorPage() {
   return (
     <div className="bg-white ">
       <main className="w-full px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div className="flex-shrink-0 pt-16">
-          <img
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
             className="w-auto h-12 mx-auto"
-            src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=600"
-            alt="Workflow"
-          />
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              className="text-pink-500"
+              d="M2 5a2 2 0 012-2h7a2 2 0 012 2v4a2 2 0 01-2 2H9l-3 3v-3H4a2 2 0 01-2-2V5z"
+            />
+            <path
+              className="text-blue-900"
+              d="M15 7v2a4 4 0 01-4 4H9.828l-1.766 1.767c.28.149.599.233.938.233h2l3 3v-3h2a2 2 0 002-2V9a2 2 0 00-2-2h-1z"
+            />
+          </svg>
         </div>
         <div className="max-w-xl py-16 mx-auto sm:py-24">
           <div className="text-center">
