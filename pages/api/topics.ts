b17bceca3db98280611139b7489c21a7fb889d4f
Jesse Klotz <jesseklotz2306@gmail.com>
2021-11-28 16:21:22 +0100

    Update topics

--- pages/api/topics.ts
+++ pages/api/topics.ts
@@ -1,6 +1,6 @@
 import prisma from "../../lib/prisma";
 
 export default async function handler(req, res) {
-  const topics = await prisma.topic.findMany({});
+  const topics = await prisma.topic.findMany({}); 
   return res.status(200).json(topics);
 }
