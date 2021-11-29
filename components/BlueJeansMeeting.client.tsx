267a07defc656c2657909823f5b294b35574910c
Jesse Klotz <jesseklotz2306@gmail.com>
2021-11-29 09:13:53 +0100

    Updated broken link params

--- components/BlueJeansMeeting.client.tsx
+++ components/BlueJeansMeeting.client.tsx
@@ -9,7 +9,7 @@ export const BlueJeansMeeting = () => {
   return (
     <iframe
       title="BlueJeans"
-      src={`https://bluejeans.com/784330684/quick?embed=true&amp;chat=false&amp;ll=de&amp;parent=https://www.network-day.de&amp;sign_in=false&amp;footer=false&amp;apps=false&amp;room_pairing=false&amp;layout=true&amp;copy_link=false&amp;rating=false&amp;app_pitch=false&amp;lockMeetingControls=true&amp;locale=de&amp;name=${session.data.user.name}&amp;email=${session.data.user.email}`}
+      src={`https://bluejeans.com/784330684/quick?embed=true&chat=false&ll=de&parent=https://www.network-day.de&sign_in=false&footer=false&apps=false&room_pairing=false&layout=true&copy_link=false&rating=false&app_pitch=false&lockMeetingControls=true&locale=de&name=${session.data.user.name}&email=${session.data.user.email}`}
       allow="autoplay; fullscreen; microphone; camera; display-capture"
       className="p-2 rounded-3xl"
       width="1920px"
