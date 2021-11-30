37fce340156f6628ae8ab40379cb81d891c5686d
Jesse Klotz <jesseklotz2306@gmail.com>
2021-11-30 11:56:58 +0100

    Add Head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,20 @@
 import type { AppProps } from "next/app";
-
+import Head from "next/head";
 import "../styles/globals.css";
 
 function NetworkDayApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Head>
+        <title>#NetworkDay</title>
+        <meta name="description">
+          Das Digital Event zur Nachhaltigkeit als Wirtschaftsfaktor im
+          Außer-Haus-Markt.
+        </meta>
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default NetworkDayApp;
