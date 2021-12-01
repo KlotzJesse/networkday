41c7243dcc3ef728413e4753e9cbf7dcb846e6bf
Jesse Klotz <jesseklotz2306@gmail.com>
2021-12-01 10:33:53 +0100

    Include relation

--- pages/api/speakers.ts
+++ pages/api/speakers.ts
@@ -1,6 +1,8 @@
 import prisma from "../../lib/prisma";
 
 export default async function handler(req, res) {
-  const speaker = await prisma.speaker.findMany({});
+  const speaker = await prisma.speaker.findMany({include: {
+      Topic: true
+  }});
   return res.status(200).json(speaker);
 }
--- pages/api/topics.ts
+++ pages/api/topics.ts
@@ -1,6 +1,8 @@
 import prisma from "../../lib/prisma";
 
 export default async function handler(req, res) {
-  const topics = await prisma.topic.findMany({}); 
+  const topics = await prisma.topic.findMany({include: {
+    speaker: true
+  }}); 
   return res.status(200).json(topics);
 }
