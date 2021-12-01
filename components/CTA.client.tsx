3d269cb6f33d0a47e5c3f6a64bf92e4b1ddf9650
Jesse Klotz <jesseklotz2306@gmail.com>
2021-12-01 04:41:29 +0100

    Add CTA

--- components/CTA.client.tsx
+++ components/CTA.client.tsx
@@ -4,15 +4,15 @@ import React from "react";
 const CTA = () => {
   return (
     <div className="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
-      <div className="flex w-full bg-gray-800 not-prose rounded-2xl">
-        <div className="w-1/2 bg-indigo-600 rounded-2xl">
+      <div className="flex flex-col w-full bg-gray-800 md:flex-row not-prose rounded-2xl">
+        <div className="bg-indigo-600 md:w-1/2 rounded-2xl">
           <img
             className="object-cover w-full h-full rounded-2xl"
             src="https://images.unsplash.com/photo-1505373877841-8d25f7d46678?i?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60&blend=6366F1&sat=-100&blend-mode=multiply"
             alt=""
           />
         </div>
-        <div className="w-1/2 px-4 py-12 sm:px-6 lg:px-8 lg:py-16">
+        <div className="px-4 py-12 md:w-1/2 sm:px-6 lg:px-8 lg:py-16">
           <div className="md:pl-10">
             <h2 className="text-base font-semibold tracking-wider text-gray-300 uppercase">
               Jetzt netzwerken
