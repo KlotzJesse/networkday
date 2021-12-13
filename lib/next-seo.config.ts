c2e2a39040bd625e8ccdabee6d40d8184c0f869e
Jesse Klotz <jesseklotz2306@gmail.com>
2021-12-13 20:30:19 +0100

    Improve SEO & meta tags

---
+++ lib/next-seo.config.ts
@@ -0,0 +1,18 @@
+const SEO = {
+    title: 'Digitales Netzwerken rundum Gastronomie, Hotel, Krankenhaus & Pflegeheim',
+    titleTemplate: '%s - NetworkDay',
+    description: 'Das Digital Event zur Nachhaltigkeit als Wirtschaftsfaktor im Außer-Haus-Markt.',
+    openGraph: {
+      type: 'website',
+      locale: 'de_DE',
+      url: 'https://www.network-day.de',
+      site_name: 'NetworkDay',
+    },
+    twitter: {
+      handle: '@handle',
+      site: '@site',
+      cardType: 'summary_large_image',
+    },
+  };
+
+  export default SEO;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
+import SEO from "@lib/next-seo.config";
 import "@styles/globals.css";
+import { DefaultSeo } from "next-seo";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 
@@ -6,13 +8,17 @@ function NetworkDayApp({ Component, pageProps }: AppProps) {
   return (
     <div>
       <Head>
-        <title>#NetworkDay</title>
-
-        <meta
+        {/* <title>#NetworkDay</title> */}
+        <meta charSet="utf-8"></meta>
+        <meta httpEquiv="Content-Language" content="de" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        {/* <meta
           name="description"
           content="Das Digital Event zur Nachhaltigkeit als Wirtschaftsfaktor im Außer-Haus-Markt."
-        ></meta>
+        ></meta> */}
       </Head>
+      <DefaultSeo {...SEO} />
+
       <Component {...pageProps} />
     </div>
   );
