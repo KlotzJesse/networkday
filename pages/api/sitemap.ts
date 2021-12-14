26ca4a2f6b76596adcd6f96f2c73679f0e706b59
Jesse Klotz <jesseklotz2306@gmail.com>
2021-12-14 02:24:02 +0100

    Last mod

--- pages/api/sitemap.ts
+++ pages/api/sitemap.ts
@@ -7,6 +7,7 @@ const createSitemap = (links, posts) => `<?xml version="1.0" encoding="UTF-8"?>
             return `
                 <url>
                     <loc>${`https://www.network-day.de/${link.full_slug}`}</loc>
+                    <lastmod>${link.published_at.split("T")[0]}</lastmod>
                 </url>
             `;
           })
@@ -16,13 +17,14 @@ const createSitemap = (links, posts) => `<?xml version="1.0" encoding="UTF-8"?>
               return `
                   <url>
                       <loc>${`https://www.network-day.de/${post.full_slug}`}</loc>
+                      <lastmod>${post.published_at.split("T")[0]}</lastmod>
                       <news:news>
    <news:publication>
      <news:name>NetworkDay</news:name>
      <news:language>de</news:language>
    </news:publication>
    <news:genres>Blog</news:genres>
-   <news:publication_date>${post.published_at}</news:publication_date>
+   <news:publication_date>${post.published_at.split("T")[0]}</news:publication_date>
      <news:title>${post.name}</news:title>
     </news:news>
                   </url>
@@ -34,6 +36,7 @@ const createSitemap = (links, posts) => `<?xml version="1.0" encoding="UTF-8"?>
 
 export default async function handler(req, res) {
     var data = await (await fetch(`https://api.storyblok.com/v1/cdn/stories?token=wM296GLYg0BtNHBdvqcsuwtt&excluding_slugs=*blog*&cv=${new Date().getTime()}`)).json();
+
 
     var blog = await fetchData("/api/components/blog");
 
