f4cb9c17ed5a914c658dc2a884ab4e710348ae01
Jesse Klotz <jesseklotz2306@gmail.com>
2022-02-21 08:12:12 +0100

    Update date

--- components/Steps.client.tsx
+++ components/Steps.client.tsx
@@ -7,7 +7,7 @@ export default function Step() {
             <dl className="bg-white rounded-lg shadow-lg sm:grid sm:grid-cols-3">
               <div className="flex flex-col p-6 text-center border-b border-gray-100 sm:border-0 sm:border-r">
                 <dt className="order-2 mt-2 text-lg font-medium leading-6 text-gray-600">
-                  23.02.2022 ab 14:00 Uhr bis 15:00 Uhr
+                  30.03.2022 ab 14:00 Uhr bis 15:00 Uhr
                 </dt>
                 <dd className="order-1 text-5xl font-extrabold text-indigo-600">
                   Wann?
